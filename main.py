f4ed712fb4f9df484e6ad599fb90d482bee144cc
Alan Newton <128156285+newtonalan@users.noreply.github.com>
2023-04-03 11:30:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,28 +18,22 @@ if g == 1:
         
 
 elif (g == 2):
-    print("A equação é do segundo grau")
-    print(" ")
     a = float(input("Digite o valor de A: \n"))
-    print(" ")
     if a == 0:
         print("Valor de a inválido")
     else:    
         b = float(input("Digite o valor de B: \n"))
         c = float(input("Digite o valor de C: \n"))
+
         if ((b ** 2) - (4 * a * c)) < 0:
             print("A equação não possui raízes reais")
         elif ((b ** 2) - (4 * a * c)) == 0:
-            print("A equação possui apenas uma raiz real")
-            print(" ")
             r = (-b + (math.sqrt((b**2) - (4 * a * c)))) / 2 * a
-            print(f"A raiz da equação '{a}X² + {b}x + {c} = 0' é {r:.2f}")
+            print(f"A equação possui apenas uma raiz real, {r:.2f}")
         elif ((b ** 2) - (4 * a * c)) > 0:
-            print("A equação possui duas raizes reais")
-            print(" ")
-            r = (-b + (math.sqrt((b**2) - (4 * a * c)))) / 2 * a
-            print(f"A raiz da equação '{a}X² + {b}x + {c} = 0' é {r:.2f}")
-        
+            r1 = (-b + (math.sqrt((b**2) - (4 * a * c)))) / 2 * a
+            r2 = (-b + (math.sqrt((b**2) - (4 * a * c)))) / 2 * a
+            print(f"A equação possui duas raizes reais, {r1:.2f}, {r2:.2f}")
 
     
 else:
